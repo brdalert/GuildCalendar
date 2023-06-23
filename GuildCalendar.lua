ab4dd22d3a03c28f1daccdd4e141bacdc23b703c
Brandon Dunn <brandondunn_21@yahoo.com>
2023-06-22 21:58:28 -0500

    working on adding loggin and better coding practices

--- GuildCalendar.lua
+++ GuildCalendar.lua
@@ -1,13 +1,60 @@
 local em = GetEventManager()
 local wm = GetWindowManager()
 local _
-GuildCalendar = {}
+
+-- Namespace for this addon
+if GuildCalendar == nil then  GuildCalendar = {} end
+local GuildCalendar = GuildCalendar
+
+-- Basic values
 GuildCalendar.name = "GuildCalendar"
+GuildCalendar.version = "0.0.1"
+
+-- Logger 
+local mainlogger
+local subloggers = {}
+local LOG_LEVEL_VERBOSE = "V"
+local LOG_LEVEL_DEBUG = "D"
+local LOG_LEVEL_INFO = "I"
+local LOG_LEVEL_WARNING ="W"
+local LOG_LEVEL_ERROR = "E"
+
+if LibDebugLogger then
+
+	mainlogger = LibDebugLogger.Create(GuildCalendar.name)
+
+	LOG_LEVEL_VERBOSE = LibDebugLogger.LOG_LEVEL_VERBOSE
+	LOG_LEVEL_DEBUG = LibDebugLogger.LOG_LEVEL_DEBUG
+	LOG_LEVEL_INFO = LibDebugLogger.LOG_LEVEL_INFO
+	LOG_LEVEL_WARNING = LibDebugLogger.LOG_LEVEL_WARNING
+	LOG_LEVEL_ERROR = LibDebugLogger.LOG_LEVEL_ERROR
+
+  subloggers["main"] = mainlogger
+  subloggers["UI"] = mainlogger:Create("UI")
+end
+
+
+local function Print(catagory, level)
+  if mainlogger == nil then return end
+
+  local logger = category and subloggers[catagory] or mainlogger
+
+  if type(logger.Log) =="function" then logger:log(level, ...) end
+end
+
+GuildCalendar.Print = Print
+
+function GuildCalendar.GetDebugLevels()
+
+	return 	LOG_LEVEL_VERBOSE, LOG_LEVEL_DEBUG, LOG_LEVEL_INFO, LOG_LEVEL_WARNING, LOG_LEVEL_ERROR
+
+end
+
 
 function GuildCalendar.Initialize()
   GuildCalendar.inCombat = IsUnitInCombat("player")
  
-  EVENT_MANAGER:RegisterForEvent(GuildCalendar.name, EVENT_PLAYER_COMBAT_STATE, GuildCalendar.OnPlayerCombatState)
+  em:RegisterForEvent(GuildCalendar.name, EVENT_PLAYER_COMBAT_STATE, GuildCalendar.OnPlayerCombatState)
  
   GuildCalendar.savedVariables = ZO_SavedVars:NewCharacterIdSettings("GuildCalendarSavedVariables", 1, nil, {})
  
@@ -19,7 +66,7 @@ function GuildCalendar.OnAddOnLoaded(event, addonName)
     if addonName == GuildCalendar.name then
         GuildCalendar.Initialize()
       --unregister the event again as our addon was loaded now and we do not need it anymore to be run for each other addon that will load
-      EVENT_MANAGER:UnregisterForEvent(GuildCalendar.name, EVENT_ADD_ON_LOADED) 
+      em:UnregisterForEvent(GuildCalendar.name, EVENT_ADD_ON_LOADED) 
     end
 end
 
@@ -47,5 +94,6 @@ function GuildCalendar.RestorePosition()
   GuildCalendarIndicator:ClearAnchors()
   GuildCalendarIndicator:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, left, top)
 end
-  
-em:EVENT_MANAGER:RegisterForEvent(GuildCalendar.name, EVENT_ADD_ON_LOADED, GuildCalendar.OnAddOnLoaded)
+
+-- register event handler function to initialize when addon is loaded
+em:RegisterForEvent(GuildCalendar.name, EVENT_ADD_ON_LOADED, GuildCalendar.OnAddOnLoaded)
